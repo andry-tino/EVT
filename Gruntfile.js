826f13011d7dde63881535e237b12f49a419cc45
Andrea <andry.tino@gmail.com>
2015-07-19 23:50:53 +0200

    Improvements in build
    More improvements for building.
    Notes: Still not stable/complete.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,15 @@
  * Grunt configuration.
  * Remarks: Supported platforms:
  * - Windows
+ * - Unix-based (not tested)
  */
 
 module.exports = function(grunt) {
-  // Project configuration.
-  grunt.initConfig({
+  var config = {
     pkg: grunt.file.readJSON('package.json'),
+    
     tsc: 'node node_modules/typescript/bin/tsc.js',
+    
     outName: '<%= pkg.name %>-out',
     outServerName: '<%= pkg.name %>-srv-out',
     
@@ -19,18 +21,30 @@ module.exports = function(grunt) {
       },
       compileServer: {
         command: '<%= tsc %> --project src/server'
+      },
+      compileAll: {
+        command: ''
       }
     },
     
     // Copy
     copy: {
       examples: {
-        files: [
-          { src: ['out/<%= outName %>.min.js'], dest: 'examples/evt/<%= outName %>.min.js' },
-          { src: ['out/<%= outName %>.js'], dest: 'examples/evt/<%= outName %>.js' },
-          { src: ['out/<%= outServerName %>.min.js'], dest: 'examples/evt/<%= outServerName %>.min.js' },
-          { src: ['out/<%= outServerName %>.js'], dest: 'examples/evt/<%= outServerName %>.js' }
-        ],
+        def: {
+          files: [
+            { src: ['out/<%= outName %>.min.js'], dest: 'examples/evt/<%= outName %>.min.js' },
+            { src: ['out/<%= outName %>.js'], dest: 'examples/evt/<%= outName %>.js' }
+          ]
+        },
+        srv: {
+          files: [
+            { src: ['out/<%= outServerName %>.min.js'], dest: 'examples/evt/<%= outServerName %>.min.js' },
+            { src: ['out/<%= outServerName %>.js'], dest: 'examples/evt/<%= outServerName %>.js' }
+          ]
+        },
+        all: {
+          files: []
+        }
       },
     },
     
@@ -39,25 +53,55 @@ module.exports = function(grunt) {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
-      build: {
+      def: {
         files: {
-          'out/<%= outName %>.js': 'out/<%= outName %>.min.js'
+          'out/<%= outName %>.min.js': ['out/<%= outName %>.js']
         }
       },
-      buildServer: {
+      srv: {
         files: {
-          'out/<%= outServerName %>.js': 'out/<%= outServerName %>.min.js'
+          'out/<%= outServerName %>.min.js': ['out/<%= outServerName %>.js']
         }
+      },
+      all: {
+        files: {}
       }
     }
-  });
+  };
+
+  // Configuration post-initialization
+  var utils = {
+    acquire: function(dst, src) {
+      for (var key in src) {
+        dst[key] = src[key];
+      }
+    }
+  };
+  
+  (function(shell) {
+    shell.compileAll.command = [shell.compile.command, shell.compileServer.command].join('&&');
+  })(config.shell);
+  
+  (function(copy) {
+    copy.examples.all = copy.examples.def.files.concat(copy.examples.srv.files);
+  })(config.copy);
+  
+  (function(uglify) {
+    utils.acquire(uglify.all.files, uglify.def.files);
+    utils.acquire(uglify.all.files, uglify.srv.files);
+  })(config.uglify);
+
+  // Applying configuration
+  grunt.initConfig(config);
 
   // Loading packages
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-shell');
 
-  // Default task(s): build and minify
-  grunt.registerTask('default', ['shell:compile', 'shell:compileServer', 'uglify:build', 'uglify:buildServer']);
-  grunt.registerTask('build-examples', ['shell:compile', 'shell:compileServer', 'uglify:build', 'uglify:buildServer', 'copy:examples']);
+  // Tasks
+  grunt.registerTask('default', ['shell:compileAll', 'uglify:all']);
+  grunt.registerTask('build-def', ['shell:compile', 'uglify:def']);
+  grunt.registerTask('build-srv', ['shell:compileServer', 'uglify:srv']);
+  grunt.registerTask('build-examples', ['shell:compileAll', 'uglify:all', 'copy:examples:all']);
 };
