d6ca67a9ff774f6c7c8da8a99b51f7aea8abda38
Andrea <andry.tino@gmail.com>
2016-02-23 23:22:24 +0100

    Improving examples
    Improving examples with a better structure. Not 100% working, not tested
    yet.

---
+++ examples/minimal/default.ts
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2015 Andrea Tino
+ * File: default.ts
+ */
+
+import evt = require('../../src/evt');
+
+import Evt = evt.EVT.Evt;
+
+export module EVT.Examples.Minimal {
+  export class Default {
+    
+    /**
+     * Runs the example.
+     */
+    public run() {
+      var main = document.createElement('div');
+      var title = document.createElement('h1');
+      var box1 = this.createBox();
+
+      main.className = 'main';
+
+      title.textContent = 'Welcome to EVT Minimal example';
+
+      main.appendChild(title);
+      main.appendChild(box1);
+      document.body.appendChild(main);
+
+      // Start EVT
+      var evt = new Evt(document.body);
+      evt.start();
+    }
+    
+    private createBox() : HTMLElement {
+      var container = document.createElement('div');
+      container.className = 'box';
+      for (var i = 0, c = container; i < 4; i++) {
+        var n = document.createElement('div');
+        c.appendChild(n);
+        c = n;
+      }
+      return container;
+    }
+  }
+}
