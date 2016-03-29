ee9dbc3dad2712fb54c2a8f4e641b90e31f4b1d8
Andrea <andry.tino@gmail.com>
2016-03-29 23:16:44 +0200

    Adding support for compiling server

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,5 +62,6 @@ module.exports = function(grunt) {
     
     grunt.registerTask('default', def);
     grunt.registerTask('examples', examples);
+    grunt.registerTask('server', server);
   } // End of tasks
 };
